a1a24f12e363d077f8f3741bc6fc03d78e4eaa67
Carlos González Valtierra <cgv23>
2021-03-01 19:38:50 +0100

    Added box2d

--- js/game.js
+++ js/game.js
@@ -1,3 +1,14 @@
+// Declarar todos los objetos de uso comÃºn como variables por conveniencia
+var b2Vec2 = Box2D.Common.Math.b2Vec2;
+var b2BodyDef = Box2D.Dynamics.b2BodyDef;
+var b2Body = Box2D.Dynamics.b2Body;
+var b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+var b2Fixture = Box2D.Dynamics.b2Fixture;
+var b2World = Box2D.Dynamics.b2World;
+var b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+var b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+
 //preparar requestAnimationFrame y cancelAnimationFrame para su uso
 (function() {
 	var lastTime = 0;
@@ -144,38 +155,68 @@ var game = {
 }
 
 var levels = {
-	//Nievel de datos
+	// Datos de nivel
 	data:[
-		{
-			//primer nivel
-			foreground:'N1-foreground',
-			background:'N1-background',
-			entities:[]
-		},
-		{
-			//primer nivel
+	 {   // Primer nivel 
+		foreground:'N1-foreground',
+		background:'N1-background',
+		entities:[
+			{type:"ground", name:"dirt", x:500,y:440,width:1000,height:20,isStatic:true},
+			{type:"ground", name:"wood", x:185,y:390,width:30,height:80,isStatic:true},
+
+			{type:"block", name:"wood", x:520,y:380,angle:90,width:100,height:25},
+			{type:"block", name:"glass", x:520,y:280,angle:90,width:100,height:25},								
+			{type:"villain", name:"burger",x:520,y:205,calories:590},
+
+			{type:"block", name:"wood", x:620,y:380,angle:90,width:100,height:25},
+			{type:"block", name:"glass", x:620,y:280,angle:90,width:100,height:25},								
+			{type:"villain", name:"fries", x:620,y:205,calories:420},				
+
+			{type:"hero", name:"orange",x:80,y:405},
+			{type:"hero", name:"apple",x:140,y:405},
+		]
+	 },
+		{   // Segundo nivel
 			foreground:'N2-foreground',
 			background:'N2-background',
-			entities:[]
+			entities:[
+				{type:"ground", name:"dirt", x:500,y:440,width:1000,height:20,isStatic:true},
+				{type:"ground", name:"wood", x:185,y:390,width:30,height:80,isStatic:true},
+	
+				{type:"block", name:"wood", x:820,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:720,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:620,y:380,angle:90,width:100,height:25},
+				{type:"block", name:"glass", x:670,y:317.5,width:100,height:25},
+				{type:"block", name:"glass", x:770,y:317.5,width:100,height:25},				
+
+				{type:"block", name:"glass", x:670,y:255,angle:90,width:100,height:25},
+				{type:"block", name:"glass", x:770,y:255,angle:90,width:100,height:25},
+				{type:"block", name:"wood", x:720,y:192.5,width:100,height:25},	
+
+				{type:"villain", name:"burger",x:715,y:155,calories:590},
+				{type:"villain", name:"fries",x:670,y:405,calories:420},
+				{type:"villain", name:"sodacan",x:765,y:400,calories:150},
+
+				{type:"hero", name:"strawberry",x:30,y:415},
+				{type:"hero", name:"orange",x:80,y:405},
+				{type:"hero", name:"apple",x:140,y:405},
+			]
 		},
-		{
-			//primer nivel
+		{//Tercer nivel 
 			foreground:'N3-foreground',
 			background:'N3-background',
-			entities:[]
+			entities:[],
 		},
-		{
-			//primer nivel
+		{//Cuarto nivel 
 			foreground:'N4-foreground',
 			background:'N4-background',
-			entities:[]
+			entities:[],
 		},
-		{
-			//segundo nivel
+		{//Quinto nivel 
 			foreground:'N5-foreground',
 			background:'N5-background',
-			entities:[]
-		}
+			entities:[],
+		},
 	],
 	//inicializa la pantalla de seleccion de nivel
 	init:function(){
@@ -204,27 +245,218 @@ var levels = {
 	},
 	//carga todos los datos e imagenes para un nivel
 	load:function(number){
-		//declarar un nuevo objeto de nivel actual
+	   //Inicializar box2d world cada vez que se carga un nivel
+		box2d.init();
+
+		// Declarar un nuevo objeto de nivel actual
 		game.currentLevel = {number:number,hero:[]};
-		game.score = 0;
+		game.score=0;
 		$('#score').html('Score: '+game.score);
-		game.currentHero=undefined;
+		game.currentHero = undefined;
 		var level = levels.data[number];
 
-		//cargar el fondo, el primer plano y las imagenes de la honda
+
+		//Cargar las imÃ¡genes de fondo, primer plano y honda
 		game.currentLevel.backgroundImage = loader.loadImage("assets/levels/"+level.background+".png");
 		game.currentLevel.foregroundImage = loader.loadImage("assets/levels/"+level.foreground+".png");
 		game.slingshotImage = loader.loadImage("assets/images/tirachinas.png");
 		game.slingshotFrontImage = loader.loadImage("assets/images/tirachinas-front.png");
 
-		//llamar a game.start cuando lo assets se hayan cargado
-		if(loader.loaded){
-			game.start();
-		}else{
-			loader.onload = game.start;
-		}
+		// Cargar todas la entidades
+		for (var i = level.entities.length - 1; i >= 0; i--){	
+			var entity = level.entities[i];
+			entities.create(entity);			
+		};
+
+		  //Llamar a game.start() una vez que los assets se hayan cargado
+	   if(loader.loaded){
+		   game.start()
+	   } else {
+		   loader.onload = game.start;
+	   }
 	}
 }
+
+var entities = {
+	definitions:{
+		"glass":{
+			fullHealth:100,
+			density:2.4,
+			friction:0.4,
+			restitution:0.15,
+		},
+		"wood":{
+			fullHealth:500,
+			density:0.7,
+			friction:0.4,
+			restitution:0.4,
+		},
+		"dirt":{
+			density:3.0,
+			friction:1.5,
+			restitution:0.2,	
+		},
+		"burger":{
+			shape:"circle",
+			fullHealth:40,
+			radius:25,
+			density:1,
+			friction:0.5,
+			restitution:0.4,	
+		},
+		"sodacan":{
+			shape:"rectangle",
+			fullHealth:80,
+			width:40,
+			height:60,
+			density:1,
+			friction:0.5,
+			restitution:0.7,	
+		},
+		"fries":{
+			shape:"rectangle",
+			fullHealth:50,
+			width:40,
+			height:50,
+			density:1,
+			friction:0.5,
+			restitution:0.6,	
+		},
+		"apple":{
+			shape:"circle",
+			radius:25,
+			density:1.5,
+			friction:0.5,
+			restitution:0.4,	
+		},
+		"orange":{
+			shape:"circle",
+			radius:25,
+			density:1.5,
+			friction:0.5,
+			restitution:0.4,	
+		},
+		"strawberry":{
+			shape:"circle",
+			radius:15,
+			density:2.0,
+			friction:0.5,
+			restitution:0.4,	
+		},
+	},
+	// Tomar la entidad, crear un cuerpo box2d y añadirlo al mundo
+	create:function(entity){
+		var definition = entities.definitions[entity.name];	
+		if(!definition){
+			console.log ("Undefined entity name",entity.name);
+			return;
+		}	
+		switch(entity.type){
+			case "block": // Rectángulos simples
+				entity.health = definition.fullHealth;
+				entity.fullHealth = definition.fullHealth;
+				entity.shape = "rectangle";	
+				//entity.sprite = loader.loadImage("assets/images/entities/"+entity.name+".png");	
+				  entity.sprite = loader.loadImage("assets/levels/level.png");
+				//entity.breakSound = game.breakSound[entity.name];
+				box2d.createRectangle(entity,definition);				
+				break;
+			case "ground": // Rectángulos simples
+				// No hay necesidad de salud. Estos son indestructibles
+				entity.shape = "rectangle";  
+				// No hay necesidad de sprites. Éstos no serán dibujados en absoluto 
+				box2d.createRectangle(entity,definition);			   
+				break;	
+			case "hero":	// Círculos simples
+			case "villain": // Pueden ser círculos o rectángulos
+				entity.health = definition.fullHealth;
+				entity.fullHealth = definition.fullHealth;
+				//entity.sprite = loader.loadImage("assets/images/entities/"+entity.name+".png");
+				entity.sprite = loader.loadImage("assets/levels/level.png");
+				entity.shape = definition.shape;  
+				entity.bounceSound = game.bounceSound;
+				if(definition.shape == "circle"){
+					entity.radius = definition.radius;
+					box2d.createCircle(entity,definition);					
+				} else if(definition.shape == "rectangle"){
+					entity.width = definition.width;
+					entity.height = definition.height;
+					box2d.createRectangle(entity,definition);					
+				}												 
+				break;							
+			default:
+				console.log("Undefined entity type",entity.type);
+				break;
+		}		
+	}
+}
+var box2d = {
+	scale:30,
+	init:function(){
+		// Configurar el mundo de box2d que hará la mayoría de los cálculos de la física
+		var gravity = new b2Vec2(0,9.8); //Declara la gravedad como 9,8 m / s ^ 2 hacia abajo
+		var allowSleep = true; //Permita que los objetos que están en reposo se queden dormidos y se excluyan de los cálculos
+		box2d.world = new b2World(gravity,allowSleep);
+	},
+		
+	createRectangle:function(entity,definition){
+			var bodyDef = new b2BodyDef;
+			if(entity.isStatic){
+				bodyDef.type = b2Body.b2_staticBody;
+			} else {
+				bodyDef.type = b2Body.b2_dynamicBody;
+			}
+			
+			bodyDef.position.x = entity.x/box2d.scale;
+			bodyDef.position.y = entity.y/box2d.scale;
+			if (entity.angle) {
+				bodyDef.angle = Math.PI*entity.angle/180;
+			}
+			var fixtureDef = new b2FixtureDef;
+			fixtureDef.density = definition.density;
+			fixtureDef.friction = definition.friction;
+			fixtureDef.restitution = definition.restitution;
+
+			fixtureDef.shape = new b2PolygonShape;
+			fixtureDef.shape.SetAsBox(entity.width/2/box2d.scale,entity.height/2/box2d.scale);
+			
+			var body = box2d.world.CreateBody(bodyDef);	
+			body.SetUserData(entity);
+			
+			var fixture = body.CreateFixture(fixtureDef);
+			return body;
+	},
+	
+	createCircle:function(entity,definition){
+			var bodyDef = new b2BodyDef;
+			if(entity.isStatic){
+				bodyDef.type = b2Body.b2_staticBody;
+			} else {
+				bodyDef.type = b2Body.b2_dynamicBody;
+			}
+			
+			bodyDef.position.x = entity.x/box2d.scale;
+			bodyDef.position.y = entity.y/box2d.scale;
+			
+			if (entity.angle) {
+				bodyDef.angle = Math.PI*entity.angle/180;
+			}			
+			var fixtureDef = new b2FixtureDef;
+			fixtureDef.density = definition.density;
+			fixtureDef.friction = definition.friction;
+			fixtureDef.restitution = definition.restitution;
+
+			fixtureDef.shape = new b2CircleShape(entity.radius/box2d.scale);
+			
+			var body = box2d.world.CreateBody(bodyDef);	
+			body.SetUserData(entity);
+
+			var fixture = body.CreateFixture(fixtureDef);
+			return body;
+	},  
+}
+
+
 var loader ={
 	loaded:true,
 	loadedCount:0,//assets que han sido cargados antes
